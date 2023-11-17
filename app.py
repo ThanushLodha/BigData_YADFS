9715930a7b5f422cf91f06770186ef7548183821
ThanushLodha <114646388+ThanushLodha@users.noreply.github.com>
2023-11-17 23:00:07 +0530

    Completed uplaoding - Thanush J Lodha

--- app.py
+++ app.py
@@ -1,10 +1,9 @@
 from flask import Flask, render_template, request
 from werkzeug.utils import secure_filename
-import os
 import mysql.connector
 import multiprocessing
-from io import BytesIO
 import socket
+import math
 
 app = Flask(__name__)
 
@@ -13,10 +12,10 @@ def create_file_table(mysql_connection):
         cursor.execute(
             """
             CREATE TABLE IF NOT EXISTS file_metadata (
-                id INT AUTO_INCREMENT PRIMARY KEY,
-                path VARCHAR(255) NOT NULL,
+                file_id INT AUTO_INCREMENT PRIMARY KEY,
+                file_name VARCHAR(255) NOT NULL UNIQUE,
                 size VARCHAR(255) NOT NULL,
-                block_size INT NOT NULL
+                no_of_blocks INT NOT NULL
             )
             """
         )
@@ -28,45 +27,54 @@ def create_block_table(mysql_connection):
             """
             CREATE TABLE IF NOT EXISTS block_metadata (
                 id INT AUTO_INCREMENT PRIMARY KEY,
-                file_path VARCHAR(255) NOT NULL,
+                file_id INT NOT NULL,
                 block_id INT NOT NULL,
-                data_node_ip VARCHAR(15) NOT NULL,
-                block_directory VARCHAR(255) NOT NULL
+                data_node INT NOT NULL
             )
             """
         )
         mysql_connection.commit()
 
-def create_file(file_path, file_size, block_size, mysql_connection):
+def create_file(file_name,file_size,block,mysql_connection):
     with mysql_connection.cursor() as cursor:
         cursor.execute(
-            "INSERT INTO file_metadata (path, size, block_size) "
+            "INSERT INTO file_metadata (file_name,size,no_of_blocks) "
+            "VALUES (%s, %s,%s)",
+            (file_name, file_size,block)
+        )
+        mysql_connection.commit()
+
+def add_block_location(file_id,block_id,data_node,mysql_connection):
+    with mysql_connection.cursor() as cursor:
+        cursor.execute(
+            "INSERT INTO block_metadata (file_id, block_id, data_node) "
             "VALUES (%s, %s, %s)",
-            (file_path, file_size, block_size)
+            (file_id, block_id, data_node)
         )
         mysql_connection.commit()
 
-def add_block_location(file_path, block_id, data_node_ip, block_directory, mysql_connection):
-    with mysql_connection.cursor() as cursor:
-        cursor.execute(
-            "INSERT INTO block_metadata (file_path, block_id, data_node_ip, block_directory) "
-            "VALUES (%s, %s, %s, %s)",
-            (file_path, block_id, data_node_ip, block_directory)
-        )
-        mysql_connection.commit()
-
-def send_block_data_to_server(file_path, block_id, data, data_node_id):
-    server_ip = '192.168.0.112'
-    server_port = '12345'
+def send_block_data_to_server(file_path, block_id, data, data_node_id,file_id):
+    server_ip = '192.168.1.169'
+    server_port = 12345
 
     with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
-        s.connect((server_ip, int(server_port)))
+        s.connect((server_ip, server_port))
 
-        metadata = f"{file_path} {block_id} {data_node_id}\n"
+        metadata = f"{file_path} {block_id} {data_node_id} {file_id}\n"
         s.sendall(metadata.encode())
         s.sendall(data)
 
         print(f"DataNode: Sent Block-{block_id} to Server")
+
+        # fi_path = s.recv(1024).decode()
+        # print(fi_path)
+
+def get_file_id(file_name,mysqlconnection):
+    cursor = mysqlconnection.cursor()
+    query = """SELECT file_id FROM file_metadata WHERE file_name=%s"""
+    cursor.execute(query,(file_name,))
+    result = cursor.fetchone()
+    return result[0]
 
 @app.route('/', methods=['GET', 'POST'])
 def upload_file():
@@ -89,8 +97,8 @@ def upload_file():
             create_block_table(mysql_connection)
 
             filename = secure_filename(file.filename)
-
-            create_file(filename, file_size, block_size, mysql_connection)
+            no_of_blocks = math.ceil(file_size/block_size)
+            create_file(filename, file_size,no_of_blocks, mysql_connection)
 
             num_data_nodes = 3
             data_queue = multiprocessing.Queue()
@@ -110,7 +118,7 @@ def upload_file():
                 data_queue.put({"action": "store_block", "file_path": filename, "block_id": block_id, "data": block_data})
                 block_id += 1
 
-            for _ in range(num_data_nodes):
+            for i in range(num_data_nodes):
                 data_queue.put({"action": "exit"})
 
             for data_node_process in data_nodes:
@@ -135,12 +143,13 @@ def data_node(data_node_id, data_queue):
             print(f"DataNode-{data_node_id} is exiting.")
             break
 
-        action = task["action"]
         file_path = task["file_path"]
         block_id = task["block_id"]
         data = task.get("data")
+        file_id = get_file_id(file_path,mysql_connection)
 
-        send_block_data_to_server(file_path, block_id, data, data_node_id)
+        send_block_data_to_server(file_path, block_id, data, data_node_id,file_id)
+        add_block_location(file_id , block_id,data_node_id,mysql_connection)
 
 if __name__ == '__main__':
     app.run(debug=True)
--- client.py
+++ client.py
@@ -1,9 +1,41 @@
 import socket
 import os
+import mysql.connector
+
+def create_server_metadata(mysql_connection):
+    with mysql_connection.cursor() as cursor:
+        cursor.execute(
+            """
+            CREATE TABLE IF NOT EXISTS server_block_metadata (
+                id INT AUTO_INCREMENT PRIMARY KEY,
+                file_id INT NOT NULL,
+                block_id INT NOT NULL,
+                block_name VARCHAR(255) NOT NULL,
+                block_location VARCHAR(255) NOT NULL
+            )
+            """
+        )
+        mysql_connection.commit()
+
+def add_block_location(file_id,block_id,block_name,block_location,mysql_connection):
+    with mysql_connection.cursor() as cursor:
+        cursor.execute(
+            "INSERT INTO server_block_metadata (file_id, block_id, block_name, block_location) "
+            "VALUES (%s, %s, %s,%s)",
+            (file_id, block_id, block_name,block_location)
+        )
+        mysql_connection.commit()
 
 
 def receive_block_data():
-    server_address = ('192.168.0.112', 12345)
+    server_address = ('192.168.1.169', 12345)
+    mysql_connection = mysql.connector.connect(
+                host="localhost",
+                user="root",
+                password="methmonk",
+                database="bigdata",
+            )
+    create_server_metadata(mysql_connection)
 
     with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
         s.bind(server_address)
@@ -13,27 +45,30 @@ def receive_block_data():
 
         while True:
             conn, addr = s.accept()
+            # i = 0
             with conn:
                 print(f"Connection from {addr}")
-
-                # Receive metadata (file path, block id, data node id)
                 metadata = conn.recv(1024).decode().split()
-                # file_path, block_id, data_node_id = metadata
 
                 block_filename = f"client/data_node_{metadata[2]}/{metadata[0]}_block{metadata[1]}.dat"
                 os.makedirs(os.path.dirname(block_filename), exist_ok=True)
 
-                # Receive block data
+                print("Starting to read the file")
                 with open(block_filename, "wb") as block_file:
                     while True:
+                        # print(f"Writing data:{i}")
+                        # i = i + 1
                         data = conn.recv(1024)
                         if not data:
                             break
                         block_file.write(data)
+                block_directory = os.path.abspath(block_filename)
 
-                print(
-                    f"Received Block-{metadata[1]} from DataNode-{metadata[2]} and saved as {block_filename}")
-
+                print(f"Received Block-{metadata[1]} from DataNode-{metadata[2]} and saved as {block_filename}")
+                print(f"File path :{block_directory}")
+                block_name = f"{metadata[0]}_block{metadata[1]}.dat"
+                add_block_location(metadata[3],metadata[1],block_name,block_directory,mysql_connection)
+                # conn.sendall(block_directory.encode())
 
 if __name__ == "__main__":
     receive_block_data()
